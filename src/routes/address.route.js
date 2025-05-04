88cd628bd696ede60039f9058dc1311bb3be40dc
Jeet Majumder <Jeetmajumder858@gmail.com>
2025-05-04 19:14:36 +0530

    admin can get address id

--- src/routes/address.route.js
+++ src/routes/address.route.js
@@ -1,16 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const addressController = require('../controllers/address.controller.js');
-const authenticate = require('../middleware/auth.middleware');
+const addressController = require("../controllers/address.controller.js");
+const authenticate = require("../middleware/auth.middleware");
 
 // All routes require authentication
 router.use(authenticate.verifyToken);
 
 // Address routes
-router.post('/', addressController.createAddress);
-router.get('/', addressController.getAllAddresses);
-router.get('/:addressId', addressController.getAddress);
-router.put('/:addressId', addressController.updateAddress);
-router.delete('/:addressId', addressController.deleteAddress);
+router.post("/", addressController.createAddress);
+router.get("/", addressController.getAllAddresses);
+router.get("/:addressId", addressController.getAddress);
+router.get("/admin/:addressId", addressController.getAddressForAdmin);
+router.put("/:addressId", addressController.updateAddress);
+router.delete("/:addressId", addressController.deleteAddress);
 
-module.exports = router; 
+module.exports = router;
